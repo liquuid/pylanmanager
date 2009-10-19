7b14af39061fd4feffca97b47719c852496f0273
Fernando Henrique <liquuid@liquuid.net>
2009-10-18 23:09:20 -0200

    janela de configuracao escondida, ja le  e escreve do arquivo de conf local

--- pylan.py
+++ pylan.py
@@ -5,14 +5,26 @@ import gobject
 import gtk
 from datetime import datetime
 from time import time
+import sys
 import os
 
-#import threading
-#import SimpleHTTPServer
-#import SocketServer
-#import socket
-#import pickle
-#port = 3141
+try:
+	arquivo = open(os.path.expanduser('~/')+'.pylanrc','r')
+	arquivo.close()
+except IOError:
+	print "criando arquivo de configuracao"
+	arquivo = open(os.path.expanduser('~/')+'.pylanrc','w')
+	arquivo.write('pc1,192.168.0.1\npc2,192.168.0.2\n\n')
+	arquivo.close()
+
+maquinas=[]
+
+config = open(os.path.expanduser('~/')+'.pylanrc','r')
+cfd = config.read()
+for i in range(len(cfd.split('\n'))-2):
+	maquinas.append([cfd.split('\n')[i].split(',')[0],0,0,cfd.split('\n')[i].split(',')[1],True])
+	print maquinas
+	
 
 
 #   columns
@@ -20,27 +32,16 @@ import os
   COLUMN_NAME,
   COLUMN_START,
   COLUMN_TIME,
-  COLUMN_REMAIN,
   COLUMN_IP,
   COLUMN_EDITABLE
-) = range(6)
-
-# data
-listas = [
-		[ "pc01",1255926026, 0,0,"192.168.0.101", True ],
-		[ "pc02",1255926026, 0,0,"192.168.0.102", True ],
-		[ "pc03",1255926000, 0,0,"192.168.0.103", True ],
-		[ "pc04",1255925000, 0,0,"192.168.0.104", True ],
-		[ "pc05",1255924000, 0,0,"192.168.0.105", True ]
-]
-
-#teste  = [ 0,1,2,3,4,5,6,7,8,9 ]
-#pic = pickle.dumps(teste)
+) = range(5)
 
 
 class EditBox(gtk.Window):
     def __init__(self, parent=None):
         gtk.Window.__init__(self)
+	self.win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	self.win.destroy()
         try:
             self.set_screen(parent.get_screen())
         except AttributeError:
@@ -84,22 +85,19 @@ class EditBox(gtk.Window):
         button.connect("clicked", self.on_remove_item_clicked, treeview)
         hbox.pack_start(button)
 	button = gtk.Button(label="atcha")
-	button.connect("clicked",self.__showvars,None)
+	button.connect("clicked",self.__showvars)
 	hbox.pack_start(button)
 
 	self.statusbar = gtk.Statusbar()
 	vbox.pack_start(self.statusbar,False,True,0)
 	self.statusbar.show()
 	self.context_id = self.statusbar.get_context_id("context_description")
-	#self.statusbar.push(self.context_id,"text")
-	#statusbar.pop(context_id)
 
         self.show_all()
 
-    def __showvars(self,button,model):
-	while 1:
-		while gtk.events_pending():gtk.main_iteration()
-		print datetime.now()
+    def __showvars(self,widget):
+		self.destroy()
+		return 0
 
     def __create_model(self):
 
@@ -108,22 +106,19 @@ class EditBox(gtk.Window):
             gobject.TYPE_STRING,
             gobject.TYPE_STRING,
 	    gobject.TYPE_INT,
-	    gobject.TYPE_INT,
 	    gobject.TYPE_STRING,
             gobject.TYPE_BOOLEAN
        )
 
         # add items
-        for item in listas:
+        for item in maquinas:
             iter = model.append()
 
             model.set (iter,
                   COLUMN_NAME, item[COLUMN_NAME],
                   COLUMN_START, item[COLUMN_START],
 		  COLUMN_TIME,item[COLUMN_TIME],
-		  COLUMN_REMAIN, item[COLUMN_REMAIN],
 		  COLUMN_IP,item[COLUMN_IP],
-
                   COLUMN_EDITABLE, item[COLUMN_EDITABLE]
            )
         return model
@@ -160,14 +155,6 @@ class EditBox(gtk.Window):
                                editable=COLUMN_EDITABLE)
         treeview.append_column(column)
 
-        # remain column
-        renderer = gtk.CellRendererText()
-        renderer.connect("edited", self.on_cell_edited, model)
-        renderer.set_data("column", COLUMN_REMAIN)
-
-        column = gtk.TreeViewColumn("Restante", renderer, text=COLUMN_REMAIN,
-                               editable=COLUMN_EDITABLE)
-        treeview.append_column(column)
 
         # ip column
         renderer = gtk.CellRendererText()
@@ -180,16 +167,14 @@ class EditBox(gtk.Window):
 
 
     def on_add_item_clicked(self, button, model):
-	print listas
 	new_item = ["pc", int(time()),0,0,"192.168.0.0", True]
-        listas.append(new_item)
+        maquinas.append(new_item)
 
         iter = model.append()
         model.set (iter,
             COLUMN_NAME, new_item[COLUMN_NAME],
             COLUMN_START, new_item[COLUMN_START],
 	    COLUMN_TIME, new_item[COLUMN_TIME],
-	    COLUMN_REMAIN, new_item[COLUMN_REMAIN],
 	    COLUMN_IP, new_item[COLUMN_IP],
             COLUMN_EDITABLE, new_item[COLUMN_EDITABLE]
        )
@@ -204,7 +189,7 @@ class EditBox(gtk.Window):
             path = model.get_path(iter)[0]
             model.remove(iter)
 
-            del listas[ path ]
+            del maquinas[ path ]
 
 
     def on_cell_edited(self, cell, path_string, new_text, model):
@@ -214,30 +199,25 @@ class EditBox(gtk.Window):
         column = cell.get_data("column")
 
         if column == COLUMN_NAME:
-            listas[path][COLUMN_NAME] = new_text
+            maquinas[path][COLUMN_NAME] = new_text
 
-            model.set(iter, column, listas[path][COLUMN_NAME])
+            model.set(iter, column, maquinas[path][COLUMN_NAME])
 
         elif column == COLUMN_START:
             old_text = model.get_value(iter, column)
-            listas[path][COLUMN_START] = int(new_text)
+            maquinas[path][COLUMN_START] = int(new_text)
 
-            model.set(iter, column, listas[path][COLUMN_START])
+            model.set(iter, column, maquinas[path][COLUMN_START])
     	elif column == COLUMN_TIME:
 		old_text = model.get_value(iter,column)
-		listas[path][COLUMN_TIME] = int(new_text)
+		maquinas[path][COLUMN_TIME] = int(new_text)
 
-		model.set(iter,column, listas[path][COLUMN_TIME])
-    	elif column == COLUMN_REMAIN:
-		old_text = model.get_value(iter,column)
-		listas[path][COLUMN_REMAIN] = int(new_text)
-
-		model.set(iter,column, listas[path][COLUMN_REMAIN])
+		model.set(iter,column, maquinas[path][COLUMN_TIME])
     	elif column == COLUMN_IP:
 		old_text = model.get_value(iter,column)
-		listas[path][COLUMN_IP] = new_text
+		maquinas[path][COLUMN_IP] = new_text
 
-		model.set(iter,column, listas[path][COLUMN_IP])
+		model.set(iter,column, maquinas[path][COLUMN_IP])
 
 
 class Painel(gtk.Window):
@@ -272,26 +252,26 @@ class Painel(gtk.Window):
 	table.attach(vbox6,6,7,0,1)
 	table.set_col_spacing(1,20)
 	
-	for i in listas:
+	for i in maquinas:
 		label = gtk.Label(i[0])
         	vbox.pack_start(label)
-	for i in range(len(listas)):
+	for i in range(len(maquinas)):
 		self.vars.append(gtk.Entry())
         	vbox2.pack_start(self.vars[i])
 	
-	for i in range(len(listas)):
+	for i in range(len(maquinas)):
 	        button = gtk.Button(stock="Adicionar 15 minutos")
 		button.connect("clicked", self.add15,i)
         	vbox3.pack_start(button)
-	for i in range(len(listas)):
+	for i in range(len(maquinas)):
 	        button = gtk.Button(stock="Adicionar 30 minutos")
 		button.connect("clicked", self.add30,i)
         	vbox4.pack_start(button)
-	for i in range(len(listas)):
+	for i in range(len(maquinas)):
 	        button = gtk.Button(stock="Adicionar 1 hora")
 		button.connect("clicked", self.add60,i)
         	vbox5.pack_start(button)
-	for i in range(len(listas)):
+	for i in range(len(maquinas)):
 	        button = gtk.Button(stock=gtk.STOCK_CANCEL)
 		button.connect("clicked", self.cancelseasson, i)
         	vbox6.pack_start(button)
@@ -300,15 +280,13 @@ class Painel(gtk.Window):
 	self.statusbar = gtk.Statusbar()
 	hbox.pack_start(self.statusbar,True,True,0)
 	self.context_id = self.statusbar.get_context_id("context_description")
-	#self.statusbar.push(self.context_id,"text")
-	#statusbar.pop(context_id)
 	self.statusbar.show()
        	table.attach(hbox,0,7,7,8)
 
 	self.show_all()
     
     def timeout(self,i):
-    	if (int(listas[i][1])+int(listas[i][2]*60)-int(time()))/60.0 < 0:
+    	if (int(maquinas[i][1])+int(maquinas[i][2]*60)-int(time()))/60.0 < 0:
 		return True
 	else:
 		return False
@@ -319,74 +297,45 @@ class Painel(gtk.Window):
 		while gtk.events_pending():gtk.main_iteration()
 		print datetime.now()
     def cancelseasson(self,button,i):
-	print listas[i][2]
-	listas[i][1] = 0
-	print listas[i][2]
+	maquinas[i][1] = 0
 
 
     def add15(self,button,i):
 	if self.timeout(i):
-		listas[i][1] = int(time())
-		listas[i][2] = 15
+		maquinas[i][1] = int(time())
+		maquinas[i][2] = 15
 	else:
-		listas[i][2] = listas[i][2] + 15
+		maquinas[i][2] = maquinas[i][2] + 15
     def add30(self,button,i):
 	if self.timeout(i):
-		listas[i][1] = int(time())
-		listas[i][2] = 30
+		maquinas[i][1] = int(time())
+		maquinas[i][2] = 30
 	else:
-		listas[i][2] = listas[i][2] + 30
+		maquinas[i][2] = maquinas[i][2] + 30
 
     def add60(self,button,i):
 	if self.timeout(i):
-		listas[i][1] = int(time())
-		listas[i][2] = 60
+		maquinas[i][1] = int(time())
+		maquinas[i][2] = 60
 	else:
-		listas[i][2] = listas[i][2] + 60
-
-#class ServerT(threading.Thread):
-#	def __init__(self,channel,details):
-#		print 'asd' 
-#		self.channel = channel
-#		self.details = details
-#		threading.Thread.__init__(self)
-#	def run(self):
-#		print "contectado: ",self.details[0]
-#		self.channel.send(pic)
-#		for x in xrange(10):
-#			print self.channel.recv(1024)
-#		self.channel(close)
-#		print "closed connection",self.details[0] 
-
-
+		maquinas[i][2] = maquinas[i][2] + 60
 
 
 def tempo(self):
 	for i in range(len(self.vars)):
-		if (int(listas[i][1])+int(listas[i][2]*60)-int(time()))/60.0 < 1 and (int(listas[i][1])+int(listas[i][2]*60)-int(time()))/60.0 > 0:
-			self.vars[i].set_text(str((int(listas[i][1])+int(listas[i][2]*60)-int(time())))+" segundos")
+		if (int(maquinas[i][1])+int(maquinas[i][2]*60)-int(time()))/60.0 < 1 and (int(maquinas[i][1])+int(maquinas[i][2]*60)-int(time()))/60.0 > 0:
+			self.vars[i].set_text(str((int(maquinas[i][1])+int(maquinas[i][2]*60)-int(time())))+" segundos")
 		elif	self.timeout(i):
 			self.vars[i].set_text(str("Encerrado"))
 		elif 	not self.timeout(i):
-			self.vars[i].set_text(str((int(listas[i][1])+int(listas[i][2]*60)-int(time())+60)/60)+" minutos") 
+			self.vars[i].set_text(str((int(maquinas[i][1])+int(maquinas[i][2]*60)-int(time())+60)/60)+" minutos") 
 	self.statusbar.push(self.context_id,str(datetime.now()))
 	return True
 
 
 def main():
-
-#    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    server.bind(('',27272))
-#    server.listen(5)
-#    while True:
-#	    print ' d' 
-#	    channel ,details = server.accept()
-#	    print ' s' 
-#	    ServerT(channel,details).start()
-
-#    print ' r ' 
     Painel()
-    EditBox()
+#    EditBox()
     gtk.main()
 
 if __name__ == '__main__':
