05658e47fddf6d0dc8182ce6d7bb9934302294c0
Fernando Henrique <liquuid@liquuid.net>
2009-10-22 17:12:00 -0200

    removendo o django

--- django-stuff/controle/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from pylan.controle.models import *
-
-admin.site.register(Computadores)
--- django-stuff/controle/forms.py
+++
@@ -1,9 +0,0 @@
-from pylan.controle.models import *
-from django  import forms
-
-class ControleForm(forms.Form):
-	ip = forms.CharField()
-	nome = forms.CharField()
-	tempo = forms.IntegerField()
-	dateIni = forms.IntegerField()
-
