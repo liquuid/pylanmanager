1257af788f3791516d3ae9f974be4d4e9b91815c
liquuid <liquuid@vice.localdomain>
2009-11-04 20:35:12 -0200

    mais campos no form

--- pylan-users.py
+++ pylan-users.py
@@ -35,11 +35,12 @@ class Users(gtk.Window):
 	hbox4 = gtk.HBox(False,1)
 	hbox5 = gtk.HBox(False,1)
 	hbox6 = gtk.HBox(False,1)
-
+	hbox7 = gtk.HBox(False,1)
 
 	frame = gtk.Frame('Identificação')
 	frame2 = gtk.Frame('Contato')
 	frame3 = gtk.Frame('Controle')
+	frame4 = gtk.Frame('Busca')
 
 	label_name = gtk.Label("Nome")
 	label_idade = gtk.Label("Idade")
@@ -51,7 +52,7 @@ class Users(gtk.Window):
 	label_email = gtk.Label("Email")	
 	label_addr = gtk.Label("Endereço")
 	label_cep = gtk.Label("CEP")
-
+	label_search = gtk.Label("Busca por RG/CPF") 
 	label_name.set_justify(gtk.JUSTIFY_RIGHT)
 	label_name.set_justify(gtk.JUSTIFY_RIGHT)
 	
@@ -62,6 +63,7 @@ class Users(gtk.Window):
 	entry_sex=gtk.combo_box_new_text()
 	entry_sex.append_text("Feminino")
 	entry_sex.append_text("Masculino")
+	entry_search = gtk.Entry(max=10)
 
 	entry_esco  = gtk.Entry(max=20)
 	entry_tel  = gtk.Entry(max=15)
@@ -99,16 +101,26 @@ class Users(gtk.Window):
 
 	button_add = gtk.Button(stock=gtk.STOCK_ADD)
 	button_clear = gtk.Button(stock=gtk.STOCK_CLEAR)
-
+	button_search = gtk.Button(stock=gtk.STOCK_FIND)
+	vboxp.pack_start(hbox7)
 	vboxp.pack_start(hbox)
 	vboxp.pack_start(hbox5)
 	vboxp.pack_start(hbox6)
+
 	hbox.pack_start(hbox2)
 	hbox2.pack_start(frame)
 	hbox2.pack_start(frame3)
 	hbox5.pack_start(frame2)
+
+#	hbox6.pack_start(frame4)
+
 	hbox6.pack_start(button_add)
 	hbox6.pack_start(button_clear)
+
+	hbox7.pack_start(label_search)
+	hbox7.pack_start(entry_search)
+	hbox7.pack_start(button_search)
+
 	hbox3.pack_start(vbox,False,False,5)
 	hbox3.pack_start(vbox2)
 
