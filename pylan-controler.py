c31d60457e8570d491b5241cabc64585cdb36408
liquuid <liquuid@vice.(none)>
2009-11-23 05:49:17 -0200

    Refatoração e simplificação do código do pylan-controler, filtros de segurança e maior latência na comunicação com o servidor

--- pylan-controler.py
+++ pylan-controler.py
@@ -2,18 +2,16 @@
 
 import gtk,gobject,os
 import urllib,pylanrc
+import socket
 server_ip = pylanrc.get_serverip()
 interface = pylanrc.get_interface()
 ip = pylanrc.get_ip()
 
 print 'http://'+server_ip+'/'+ip
 
-#if get_time():
-#	hud = get_time()
 hud = 0
 gnome_on = False
 first_run=False
-time = 0
 
 try:
 	os.system('qiv --root /srv/pylan/bg.png')
@@ -28,52 +26,42 @@ class Clock(gtk.Window):
 		except AttributeError:
 			self.connect('destroy',lambda *w:gtk.main_quit())
 		self.set_title('Tempo Restante')
-		self.timer = gobject.timeout_add(1000,tempo,self)
+		self.timer = gobject.timeout_add(10000,tempo,self)
 		self.set_border_width(5)
 		self.set_default_size(250,30)
 		self.entry = gtk.Statusbar()
 		self.context_id= self.entry.get_context_id('bla')
 		self.add(self.entry)
 def get_time():
-	try:
+	try:	
+		socket.setdefaulttimeout(1)
 		h = urllib.urlopen('http://'+server_ip+'/'+ip)
-		hud=h.read()
-		return hud
+		hud=h.read().strip()
+		return int(hud)
 	except IOError: 
 		print 'Sem contato com o servidor'
 
 def tempo(self):
-	global time
 	global gnome_on
 	global hud
 	global first_run
-#	try:
-#		h = urllib.urlopen('http://'+server_ip+'/'+ip)
-#		hud=h.read()
-#	except IOError: 
-#		print 'Sem contato com o servidor'
-	if get_time():
-		hud = get_time()
-		hud = int(hud.strip())
-		time = int(hud)
-		hor = str(hud/3600)
-		min = str((hud%3600)/60)
-		seg = str((hud%3600)%60)
-		hud = str(n(hor)+':'+n(min)+':'+n(seg))
+	hud = get_time()
+	if type(hud) == int: 
+		print hud
+		print type(hud)	
+		if int(hud) != 0 and not gnome_on:
+			print "abrindo gnome"
+			os.system('/srv/pylan/pylan-clock.py &')
+			os.system('gnome-session ?> /dev/null &')
+			gnome_on = True
+			first_run=True
+		if int(hud) == 0 and first_run:
+			print "fechando gnome"
+			killprocess() 
+			gnome_on = False
 	
-	if time != 0 and not gnome_on:
-		print "abrindo gnome"
-		os.system('/srv/pylan/pylan-clock.py &')
-		os.system('gnome-session ?> /dev/null &')
-		gnome_on = True
-		first_run=True
-	if time == 0 and first_run:
-		print "fechando gnome"
-		killprocess() 
-		gnome_on = False
-		
-
-	self.entry.push(self.context_id,str(hud))
+		print 't='+str(hud)+'g='+str(gnome_on)+'f='+str(first_run)
+		self.entry.push(self.context_id,str(hud))
 	return True
 
 def killprocess():
