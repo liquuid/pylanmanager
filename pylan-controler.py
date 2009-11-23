cb915154049d0a8e916dc15c2f273cce883ae3be
liquuid <liquuid@vice.(none)>
2009-11-23 08:39:52 -0200

    Timer paralelo no pylan-controler

--- pylan-controler.py
+++ pylan-controler.py
@@ -12,7 +12,7 @@ print 'http://'+server_ip+'/'+ip
 hud = 0
 gnome_on = False
 first_run=False
-
+time=0
 try:
 	os.system('qiv --root /srv/pylan/bg.png')
 except:
@@ -28,6 +28,7 @@ class Clock(gtk.Window):
 			self.connect('destroy',lambda *w:gtk.main_quit())
 		self.set_title('Tempo Restante')
 		self.timer = gobject.timeout_add(10000,tempo,self)
+		self.internal = gobject.timeout_add(1000,internal_clock,self)
 
 def get_time():
 	""" funcao que contacta o servidor e retorna o tempo restante da sessao """
@@ -38,6 +39,17 @@ def get_time():
 		return int(hud)
 	except IOError: 
 		print 'Sem contato com o servidor'
+		return 'Sem contato com o servidor'
+
+def internal_clock(self):
+	""" funcao que conta o tempo em paralelo com o servidor """
+	global time
+	if time >0:
+		time = time - 1
+	if time == 0 and first_run:
+		killprocess()
+	print time
+	return True
 
 def tempo(self):
 	""" Loop executado a cada 10 segundos, responsavel por toda verificacao
@@ -45,24 +57,26 @@ def tempo(self):
 	global gnome_on
 	global hud
 	global first_run
+	global time
 	hud = get_time()
 	if type(hud) == int: 
 		if int(hud) != 0 and not gnome_on:
 			print "abrindo gnome"
+			time = hud
 			os.system('/srv/pylan/pylan-clock.py &')
 			os.system('gnome-session ?> /dev/null &')
 			gnome_on = True
 			first_run=True
 		if int(hud) == 0 and first_run:
-			print "fechando gnome"
 			killprocess() 
 			gnome_on = False
-	
+		time = hud
 		print 't='+str(hud)+'g='+str(gnome_on)+'f='+str(first_run)
 	return True
 
 def killprocess():
 	""" Rotina para encerrar a sessao """
+	print "fechando gnome"
 	exit()	
 
 def main():
